a9fc41be323fe93c670d8ade9a9c2ac553f2d9a1
Veto Ramirez <vetorobert3@gmail.com>
2019-10-08 20:01:08 -0700

    Got rid of a unnecessary file and made a usestate hook for team members.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [members, setMember] = useState()
+
   return (
     <div className="App">
       <header className="App-header">
